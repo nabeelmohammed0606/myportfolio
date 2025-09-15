c92b435322e49469a77d46d4444032735afe02de
Nabeel Mohammed <nabeelmohammed0606@gmail.com>
2025-09-15 13:05:50 -0400

    first working version, have to get the layouts up and working

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//testing purposes:
+use App\Models\Task;
+use App\Models\User;
+
+// Route::get('/title', function () {
+//     return view('test', ['titles' => User::all()]);
+// });
+//////////////////////////////////////////////////////
+
+// TEST
+Route::get('/test', function () {
+    return view('test');
+});
+// HOME
+Route::get('/', function () {
+    return view('index');   
+})->name('home');
+// ABOUT
+Route::get('/about', function () {
+    return view('about');  
+})->name('about');
+// RESUME
+Route::get('/resume', function () {
+    return view('resume'); 
+})->name('resume');
+// SERVICES
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+// PORTFOLIO
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
